0dbd9f1f74f9d091be9fba728d1c291f9d776d01
kako351 <h3.birth@gmail.com>
2021-11-25 15:34:48 +0900

    di koin

--- app/src/main/java/com/kako351/android/dimigrationsample/KoinViewModel.kt
+++ app/src/main/java/com/kako351/android/dimigrationsample/KoinViewModel.kt
@@ -1,7 +1,9 @@
 package com.kako351.android.dimigrationsample
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class KoinViewModel: ViewModel() {
-
+    val text = MutableLiveData("KoinViewModel")
 }
--- app/src/main/java/com/kako351/android/dimigrationsample/MainActivity.kt
+++ app/src/main/java/com/kako351/android/dimigrationsample/MainActivity.kt
@@ -2,10 +2,17 @@ package com.kako351.android.dimigrationsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.kako351.android.dimigrationsample.databinding.ActivityMainBinding
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+    private val viewModel by viewModel<KoinViewModel>()
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding?.viewModel = viewModel
     }
 }
