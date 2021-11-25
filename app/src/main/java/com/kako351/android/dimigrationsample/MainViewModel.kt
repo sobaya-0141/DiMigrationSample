3a06ef42f6e186fff7fea68c7909ac09dcbf1df4
kako351 <h3.birth@gmail.com>
2021-11-25 17:20:04 +0900

    KoinComponent inject

--- app/src/main/java/com/kako351/android/dimigrationsample/MainViewModel.kt
+++ app/src/main/java/com/kako351/android/dimigrationsample/MainViewModel.kt
@@ -5,9 +5,13 @@ import androidx.lifecycle.ViewModel
 import com.kako351.android.dimigrationsample.repository.DaggerRepository
 import com.kako351.android.dimigrationsample.repository.KoinRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import org.koin.core.KoinComponent
+import org.koin.core.inject
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(val pository: DaggerRepository): ViewModel() {
+class MainViewModel @Inject constructor(val pository: DaggerRepository): ViewModel(), KoinComponent {
+    private val koinRepository: KoinRepository by inject()
+
     val text = MutableLiveData(pository.text)
 }
