32b01033697f5f1ff7452a5b75f99bbdc82ac7e7
kako351 <h3.birth@gmail.com>
2021-11-25 15:00:26 +0900

    new repository

---
+++ app/src/main/java/com/kako351/android/dimigrationsample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kako351.android.dimigrationsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
